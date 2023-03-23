f38b82818b010b303ee64f670205c70eca948777
DEREK-DEV-AFK <sufiyanmemon687@gmail.com>
2023-03-23 17:02:13 +0530

    updated upgrade proxy contract script

--- scripts/upgrade-proxy-contract.js
+++ scripts/upgrade-proxy-contract.js
@@ -2,9 +2,10 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-  const PROXY_CONTRACT_ADDRESS = '';  
+  const PROXY_CONTRACT_ADDRESS = '0x928d73c67A934781A54d5D8e9f3B7D9C9D75b68B';
+  const version = "V2";  
   const NFTMarketplaceV2 = await ethers.getContractFactory("NFTMarketplaceV2");
-  const marketplaceContractV2 = await upgrades.upgradeProxy(PROXY_CONTRACT_ADDRESS, NFTMarketplaceV2);
+  const marketplaceContractV2 = await upgrades.upgradeProxy(PROXY_CONTRACT_ADDRESS, NFTMarketplaceV2,{ initializer: [version] });
   console.log("Marketplace upgraded");
 }
 
